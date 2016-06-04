6b9885d77fe7bed61761fbda4085ca44bc6c56cb
Mark Cronje <markcronje@Marks-MacBook-Air.local>
2016-06-04 16:49:30 +1000

    API required and search tool working

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,7 @@
 class VenuesController < ApplicationController
+
+  def result
+    @search_result = HTTParty.get("https://developers.zomato.com/api/v2.1/search?q=#{params[:search]}&count=10", :headers => {"X-Zomato-API-Key" => "228c037c226e4f7fdac58a2a97f123dc"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
        root to: "home#index"
 
+       resources :venues
+
+
+
+        post '/result' => 'venues#result'
+        get '/result' => 'venues#result'
+
+        get '/search' => 'venues#search'
+
+
+
 end
